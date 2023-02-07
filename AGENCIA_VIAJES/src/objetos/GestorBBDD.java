bc84f588a4f728770d35432dd81ac486e227e3c2
Julen Cano <Julen Cano@HZ321158.H012108.NET>
2023-02-07 09:49:14 +0100

    gestorBBDD actualizado

--- AGENCIA_VIAJES/src/objetos/GestorBBDD.java
+++ AGENCIA_VIAJES/src/objetos/GestorBBDD.java
@@ -1,5 +1,20 @@
 package objetos;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class GestorBBDD extends Conector {
-	
+	public void crearReserva(Reserva reserva) {
+		try {
+			PreparedStatement crearReserva = conector
+					.prepareStatement("INSERT INTO reservas (id_habitacion,dni,desde,hasta) VALUES (?,?,?,?)");
+			crearReserva.setInt(1, reserva.getId_habitacion());
+			crearReserva.setString(2, reserva.getDni());
+			crearReserva.setString(3, reserva.getDesde());
+			crearReserva.setString(4, reserva.getHasta());
+			crearReserva.execute();
+		} catch (SQLException e) {
+			System.out.println("Error: no se ha podido crear la reserva");
+		}
+	}
 }
